b8960a870d86f0b4982ec7cfc89446e11e5e2bcc
Roman Tkachuk <tkachukroman64@gmail.com>
2020-11-08 22:28:17 +0200

    Added first lesson :3

---
+++ Lesson_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// int a; -- sozdat peremennyiu a
+// cin >> a; -- polozhit chislo
+// cout << "privet"; -- vivesty privet
+// if ( uslovie) { deistvie; }
+// while (uslovie) { deistvie; }
+
+int main()
+{
+    int a = 0;
+    while ( a < 10 )
+    {
+        cout << "*";
+        a++;
+    }
+}
